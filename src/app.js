e62f3e8eea34fd68c1661f659a759533a645f063
TXBIT <shenlong203@gmail.com>
2020-07-18 15:16:03 +0700

    002 Exploring JSX

--- src/app.js
+++ src/app.js
@@ -1,7 +1,30 @@
 console.log('App.js is running!');
 
 // JSX - JavaScript XML
-var template = <h1>Indecision App</h1>;
-var appRoot = document.getElementById('app');
+var template = (
+  <div>
+    <h1>Indecision App</h1>
+    <p>This is some info</p>
+    <ol>
+      <li>Item one</li>
+      <li>Item two</li>
+    </ol>
+  </div>
+);
 
-ReactDOM.render(template, appRoot);
+// Create a templateTwo var JSX expression
+// div
+//  h1 -> Name:
+//  p -> Age:
+//  p -> Location:
+// Render templateTwo instead of template
+var templateTwo = (
+  <div>
+    <h1>Name: Name</h1>
+    <p>Age: Age</p>
+    <p>Location: Location</p>
+  </div>
+);
+
+var appRoot = document.getElementById('app');
+ReactDOM.render(templateTwo, appRoot);
