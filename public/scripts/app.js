4ff129ee437fbcc12876cbcc984057483e636863
TXBIT <shenlong203@gmail.com>
2020-07-18 15:46:58 +0700

    004 Conditional Rendering in JSX

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,15 +2,10 @@
 
 console.log('App.js is running!');
 
-// create app object title/subtitle
-// user title/subtitle in the template
-// render template
-
-// JSX - JavaScript XML
-
 var app = {
   title: 'Indecision App',
-  subtitle: 'Put your life in the hands of a computer'
+  subtitle: 'Put your life in the hands of a computer',
+  options: ['One', 'Two']
 };
 var template = React.createElement(
   'div',
@@ -20,10 +15,15 @@ var template = React.createElement(
     null,
     app.title
   ),
-  React.createElement(
+  app.subtitle && React.createElement(
     'p',
     null,
     app.subtitle
+  ),
+  React.createElement(
+    'p',
+    null,
+    app.options.length > 0 ? 'Here are your options' : 'No options'
   ),
   React.createElement(
     'ol',
@@ -46,37 +46,32 @@ var user = {
   age: 26,
   location: 'Location'
 };
-var username = 'Name2';
-var userAge = 27;
-var userLocation = 'New York';
-
+function getLocation(location) {
+  if (location) {
+    return React.createElement(
+      'p',
+      null,
+      'Location: ',
+      location
+    );
+  }
+}
 var templateTwo = React.createElement(
   'div',
   null,
   React.createElement(
     'h1',
     null,
-    user.name.toUpperCase() + '!'
+    user.name ? user.name : 'Anonymous'
   ),
-  React.createElement(
+  user.age && user.age >= 18 && React.createElement(
     'p',
     null,
     'Age: ',
     user.age
   ),
-  React.createElement(
-    'p',
-    null,
-    'Location: ',
-    user.location
-  )
+  getLocation(user.location)
 );
-// Create a templateTwo var JSX expression
-// div
-//  h1 -> Name:
-//  p -> Age:
-//  p -> Location:
-// Render templateTwo instead of template
 
 var appRoot = document.getElementById('app');
 ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,19 +1,15 @@
 console.log('App.js is running!');
 
-// create app object title/subtitle
-// user title/subtitle in the template
-// render template
-
-// JSX - JavaScript XML
-
 var app = {
   title: 'Indecision App',
   subtitle: 'Put your life in the hands of a computer',
+  options: ['One', 'Two'],
 };
 var template = (
   <div>
     <h1>{app.title}</h1>
-    <p>{app.subtitle}</p>
+    {app.subtitle && <p>{app.subtitle}</p>}
+    <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
     <ol>
       <li>Item one</li>
       <li>Item two</li>
@@ -26,23 +22,18 @@ var user = {
   age: 26,
   location: 'Location',
 };
-var username = 'Name2';
-var userAge = 27;
-var userLocation = 'New York';
-
+function getLocation(location) {
+  if (location) {
+    return <p>Location: {location}</p>;
+  }
+}
 var templateTwo = (
   <div>
-    <h1>{`${user.name.toUpperCase()}!`}</h1>
-    <p>Age: {user.age}</p>
-    <p>Location: {user.location}</p>
+    <h1>{user.name ? user.name : 'Anonymous'}</h1>
+    {user.age && user.age >= 18 && <p>Age: {user.age}</p>}
+    {getLocation(user.location)}
   </div>
 );
-// Create a templateTwo var JSX expression
-// div
-//  h1 -> Name:
-//  p -> Age:
-//  p -> Location:
-// Render templateTwo instead of template
 
 var appRoot = document.getElementById('app');
 ReactDOM.render(template, appRoot);
