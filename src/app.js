f1073df4725b7e104f068acebc5fd1e611d7bd4b
TXBIT <shenlong203@gmail.com>
2020-07-19 10:15:53 +0700

    018 Component Props

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 class IndecisionApp extends React.Component {
   render() {
+    const title = 'Indecision';
+    const subtitle = 'Put your life in the hands of a computer';
+    const options = ['Thing one', 'Thing two', 'Thing three'];
     return (
       <div>
-        <Header />
+        <Header title={title} subtitle={subtitle} />
         <Action />
-        <Options />
+        <Options options={options} />
         <AddOption />
       </div>
     );
@@ -14,8 +17,8 @@ class Header extends React.Component {
   render() {
     return (
       <div>
-        <h1>Indecision</h1>
-        <h2>Put your life in the hands of a computer</h2>
+        <h1>{this.props.title}</h1>
+        <h2>{this.props.subtitle}</h2>
       </div>
     );
   }
@@ -31,12 +34,17 @@ class Action extends React.Component {
   }
 }
 
+// Setup options props for Options component
+// Render the length of the array
+// Render new p tag for each option (set text, set key)
+
 class Options extends React.Component {
   render() {
     return (
       <div>
-        Options component here
-        <Option />
+        {this.props.options.map((option, index) => (
+          <Option key={index} option={option} />
+        ))}
       </div>
     );
   }
@@ -44,7 +52,7 @@ class Options extends React.Component {
 
 class Option extends React.Component {
   render() {
-    return <div>Option component here</div>;
+    return <div>{this.props.option}</div>;
   }
 }
 
