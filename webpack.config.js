a092c6d102169c18bdd699088c5dbb41591e48bf
TXBIT <shenlong203@gmail.com>
2020-08-09 23:16:35 +0700

    040 Source Maps with Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -16,4 +16,9 @@ module.exports = {
     ],
   },
   devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    host: '0.0.0.0',
+    port: '4000',
+  },
 };
